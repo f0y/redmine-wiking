cdb9a04fc5b4d9f1e6ee6c5b3657817e1d5bcbb0
Oleg Kandaurov <kandaurovoleg@gmail.com>
2012-06-06 22:41:40 +0400

    Rename README

--- README.rb
+++
@@ -1,6 +0,0 @@
-= WikiNG plugin for Redmine
-
-This is a fork of WikiNG plugin by Andriy Lesyuk http://projects.andriylesyuk.com/projects/wiking/wiki
-The aim of the fork is to provide Redmine 2.0 support and small enhancements.
-
-The WikiNG plugin for Redmine and ChiliProject is aimed to extend Redmine Wiki syntax by making it more developer-ready, more emotional and social...
